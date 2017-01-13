e215dd0c40f89368308be612d0d2517bc86163dd
master <manivan.iv@gmail.com>
2017-01-13 21:10:47 +0200

    add price

--- public/js/select.js
+++ public/js/select.js
@@ -1,5 +1,5 @@
 
-var data = ' {"allval" : [ {"products": "1", "materials": "1", "price": "10"}, {"products": "1", "materials": "2", "price": "11" }, {"products": "2", "materials": "1", "price": "12"}, {"products": "2", "materials": "2", "price": "13" } ]}';
+var data = ' {"allval" : [ {"products": "1", "materials": "1", "price": "6.8"}, {"products": "1", "materials": "2", "price": "6.7" }, {"products": "1", "materials": "3", "price": "6.60" }, {"products": "1", "materials": "4", "price": "6.50" }, {"products": "1", "materials": "5", "price": "6.4" }, {"products": "1", "materials": "6", "price": "6.40" }, {"products": "1", "materials": "7", "price": "6.30" }, {"products": "1", "materials": "8", "price": "6.25" }, {"products": "1", "materials": "9", "price": "6.23"}, {"products": "1", "materials": "10", "price": "7" }, {"products": "1", "materials": "11", "price": "6.50"}, {"products": "1", "materials": "12", "price": "13"} , {"products": "1", "materials": "13", "price": "7.3"} , {"products": "1", "materials": "14", "price": "6"} , {"products": "2", "materials": "1", "price": "0"} , {"products": "2", "materials": "2", "price": "0"} , {"products": "2", "materials": "3", "price": "0"}    ]}';
 
 
 function GetPrice(summ)
